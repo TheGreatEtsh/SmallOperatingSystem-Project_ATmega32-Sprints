fcce3bb078a8c0a33868612e124d0abb743d38a8
TheGreatEtsh <ahmedhesham606@icloud.com>
2023-05-30 16:03:38 +0300

    feat (SOS): Adding SOS files

---
+++ SOS/SERV/sos/sos_private.h
@@ -0,0 +1,19 @@
+/**
+ * @fileName	:	sos_private.h
+ * @Created		: 	2:08 AM Tuesday, May 30, 2023
+ * @Author		: 	Hamsters
+ * @brief		: 	Private file for Simple Operating System 
+ */ 
+
+
+#ifndef SOS_PRIVATE_H_
+#define SOS_PRIVATE_H_
+#include "sos_interface.h"
+
+static enu_sos_status_t_	sos_search_task		(uint8_t_ uint8_task_id,str_sos_task_t_* ptr_str_sos_task);
+static void					sos_sort_database	(void);
+static void					sos_system_scheduler(void);
+static void					sos_sys_tick_task	(void);
+
+
+#endif /* SOS_PRIVATE_H_ */
