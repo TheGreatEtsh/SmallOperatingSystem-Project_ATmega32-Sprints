5ffd3120871224f5c02d39b1ef00d5cfaf0c03fc
Hossam elwahsh <hacker2894@gmail.com>
2023-05-31 09:51:47 +0300

    update (sos): interface

--- SOS/SERV/sos/sos_private.h
+++ SOS/SERV/sos/sos_private.h
@@ -10,7 +10,7 @@
 #define SOS_PRIVATE_H_
 #include "sos_interface.h"
 
-static enu_sos_status_t_	sos_search_task		(uint8_t_ uint8_task_id,str_sos_task_t_* ptr_str_sos_task);
+static enu_sos_status_t_	sos_find_task		(uint8_t_ uint8_task_id, str_sos_task_t_* ptr_str_sos_task);
 static void					sos_sort_database	(void);
 static void					sos_system_scheduler(void);
 static void					sos_sys_tick_task	(void);
