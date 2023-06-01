f75a5f57bd998ad20fb01938e5117fcd8a1a5f4f
AalaaIbrahim <118854389+AalaaIbrahim@users.noreply.github.com>
2023-06-01 12:10:54 +0300

    update (btn): added deinit function

--- SOS/ECUAL/button/btn_interface.h
+++ SOS/ECUAL/button/btn_interface.h
@@ -13,8 +13,8 @@
 
 typedef enum
 {
-	BTN_STATE_PRESSED		,
-	BTN_STATE_NOT_PRESSED	,
+	BTN_STATE_NOT_PRESSED = 0	,
+	BTN_STATE_PRESSED			,
 	BTN_STATE_TOTAL
 }enu_btn_state_t_;
 
@@ -25,19 +25,35 @@ typedef enum
 	BTN_EXTERNAL_PULL_DOWN
 }enu_btn_pull_t_;
 
+typedef enum
+{
+	BTN_ACTIVATED	= 0 ,
+	BTN_DEACTIVATED  
+}enu_btn_active_state_t_;
+
+typedef enum
+{
+	BTN_MODE_INTERRUPT = 0		,
+	BTN_MODE_POLLING
+}enu_btn_read_mode_t_;
 
 typedef enum
 {
 	BTN_STATUS_OK = 0			 ,
 	BTN_STATUS_INVALID_PULL_TYPE ,
 	BTN_STATUS_INVALID_STATE	 ,
+	BTN_STATUS_INVALID_READ_MODE ,
+	BTN_STATUS_DEACTIVATED
 }enu_btn_status_code_t_;
 
 typedef struct
 {
-	enu_dio_port_t_ BTN_PORT 	  ;
-	enu_dio_pin_t_ 	BTN_PIN 	  ;
-	enu_btn_pull_t_ BTN_PULL_TYPE ;
+	enu_dio_port_t_				enu_btn_port 	  ;
+	enu_dio_pin_t_ 				enu_btn_pin 	  ;	
+	enu_btn_read_mode_t_		enu_btn_read_mode ;
+	enu_btn_pull_t_				enu_btn_pull_type ;
+	/** Read only */
+	enu_btn_active_state_t_		enu_btn_activation;	
 }str_btn_config_t_;
 
 
@@ -52,6 +68,8 @@ typedef struct
  */
 enu_btn_status_code_t_ btn_init(str_btn_config_t_* ptr_str_btn_config);
 
+enu_btn_status_code_t_ btn_deinit(str_btn_config_t_* ptr_str_btn_config);
+
 /**
  * @brief Function to read the current button state 
  * 
@@ -61,6 +79,7 @@ enu_btn_status_code_t_ btn_init(str_btn_config_t_* ptr_str_btn_config);
  * @return BTN_STATUS_OK                : When the operation is successful
  *         BTN_STATUS_INVALID_STATE     : Button structure and/or button state pointers are NULL_PTRs
  *         BTN_STATUS_INVALID_PULL_TYPE : If the pull type field in button structure is set to invalid value
+ *		   BTN_STATUS_DEACTIVATED		: If we are trying to read from a deactivated button
  */
 enu_btn_status_code_t_ btn_read(str_btn_config_t_* ptr_str_btn_config, enu_btn_state_t_* ptr_enu_btn_state);
 
